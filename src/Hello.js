0da7292ed3c4ae53cb59202267554fb1ba0adcf0
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-04-24 08:30:50 +0100

    Create Hello.js

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+// export default function Hello() {
+//   return <div>Hello World!!!</div>;
+// }
+
+class Hello extends Component {
+  render() {
+    return <div>Hello World!!!</div>;
+  }
+}
+
+export default Hello;
