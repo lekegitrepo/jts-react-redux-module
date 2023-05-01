302ddadecae733094711159ab2562b277488f50f
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-05-01 09:33:54 +0100

    Create card component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import Hello from "./Hello";
+import Card from "./Card";
 import "tachyons";
 // import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello greetings={"Hello there!!!"} />
+    <Card />
   </React.StrictMode>
 );
 
