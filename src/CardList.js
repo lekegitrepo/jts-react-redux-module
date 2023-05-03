dc374cf9c1c0da33b7cb908c87f8da03c8fbe0c9
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-05-03 08:47:47 +0100

    Create CardList component

---
+++ src/CardList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot) => {
+        return (
+          <Card
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+            key={robot.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
