c8a8c6465a924eb66ab04185a1fbc1254f301f5e
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-05-05 07:44:56 +0100

    Create SearchBox.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import CardList from "./CardList";
 import React from "react";
 import { robots } from "./robots";
-import CardList from "./CardList";
+import SearchBox from "./SearchBox";
 const App = () => {
   return (
     <div>
       <h1>RoboFriends</h1>
+      <SearchBox />
       <CardList robots={robots} />
     </div>
   );
