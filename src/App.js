bfd7d4c7e0342b09a387fa67bceba5137e3bc054
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-05-08 08:08:49 +0100

    Style searchbox component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 const App = () => {
   return (
-    <div>
+    <div className="tc">
       <h1>RoboFriends</h1>
       <SearchBox />
       <CardList robots={robots} />
