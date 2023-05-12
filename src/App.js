3a4235d876bf837ffbda4266025d906195894228
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-05-12 08:01:54 +0100

    Add sega font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import CardList from "./CardList";
 import React, { Component } from "react";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import "./App.css";
 
 class App extends Component {
   constructor() {
@@ -25,7 +26,7 @@ class App extends Component {
     // const { robots } = this.state;
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
