396e88be24b07531340484350b300e30c713d4af
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-05-18 07:37:17 +0100

    add style to Scroll component

--- src/Scroll.js
+++ src/Scroll.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Scroll = () => {
-  return;
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "5px solid black",
+        height: "800px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Scroll;
