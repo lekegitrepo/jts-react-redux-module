a35f2c8b6a6f62fd07cd77d20f940126dc3f252d
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-06-15 08:21:12 +0100

    Updates

---
+++ precache-manifest.6b2cbe95c658b7987bab40131af15729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "113b4601ba5c1328588b",
+    "url": "/jts-react-redux-module/static/css/main.ab5cbc47.chunk.css"
+  },
+  {
+    "revision": "113b4601ba5c1328588b",
+    "url": "/jts-react-redux-module/static/js/main.113b4601.chunk.js"
+  },
+  {
+    "revision": "06a002c14efbe02ccfdc",
+    "url": "/jts-react-redux-module/static/css/1.1ced529b.chunk.css"
+  },
+  {
+    "revision": "06a002c14efbe02ccfdc",
+    "url": "/jts-react-redux-module/static/js/1.06a002c1.chunk.js"
+  },
+  {
+    "revision": "07a474e8161ec1092b9f",
+    "url": "/jts-react-redux-module/static/js/runtime~main.07a474e8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/jts-react-redux-module/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a419d9006716d231bbe5b6fbe31b8d25",
+    "url": "/jts-react-redux-module/index.html"
+  }
+];
