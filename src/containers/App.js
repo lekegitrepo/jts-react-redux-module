fcaad2b7d962cd1ae6aa5991698715640c8f694a
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-06-23 08:45:28 +0100

    Add useEffect hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,9 +23,17 @@ function App() {
   //     .then((users) => this.setState({ robots: users }));
   // }
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setRobots(users));
+  });
+
   const onSearchChange = (event) => {
     setSearchfield(event.target.value);
   };
+
+  console.log("This is robots: ", robots, robots.robots);
 
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
