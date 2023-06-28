54ac2a2d7499176836e6435f8520ae84fd0c62bd
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-06-28 08:22:04 +0100

    Remove commented code

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,22 +6,8 @@ import SearchBox from "../components/SearchBox";
 import "./App.css";
 
 function App() {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     robots: [],
-  //     searchfield: "",
-  //   };
-  // }
-
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
-
-  // componentDidMount() {
-  //   fetch("https://jsonplaceholder.typicode.com/users")
-  //     .then((response) => response.json())
-  //     .then((users) => this.setState({ robots: users }));
-  // }
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
