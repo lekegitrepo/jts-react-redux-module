4de2228846ef01160fa0096fdb532804918878a0
Temitayo Adeleke <lekedevrepo@gmail.com>
2023-09-19 08:01:21 +0100

    Debugging request action

--- src/actions.js
+++ src/actions.js
@@ -14,7 +14,10 @@ export const requestRobots = () => (dispatch) => {
   dispatch({ type: REQUEST_ROBOTS_PENDING });
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
-    .then((data) => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .then((data) => {
+      console.log("This is requestRobots: ", data);
+      dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data });
+    })
     .catch((error) =>
       dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error })
     );
